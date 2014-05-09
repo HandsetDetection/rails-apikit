21a44c85b827f5ba8867eeabf22d8e56d9a5cb4f
Jessie S. Labadan <jessielabadan07@gmail.com>
2014-05-09 14:48:58 +0800

    Added docs and test frameworks

---
+++ test/functional/handset_controller_test.rb
@@ -0,0 +1,13 @@
+require 'test_helper'
+require 'fileutils'
+
+class HandsetControllerTest < ActionController::TestCase
+
+	test load_device do
+	    #get :index
+	    #assert_response :success
+		#assert_not_nil assigns(:posts)
+		assert true
+	end
+
+end
---
+++ test/integration/handset_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class HandsetControllerTest < ActionDispatch::IntegrationTest
+	test load_device do
+  		assert true
+  	end
+end
